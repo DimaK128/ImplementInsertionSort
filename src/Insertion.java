fe1711021f17b67dcc05101c89072aabc4aaed94
DimaK128 <dmytro.kolesnyk@auk.edu.ua>
2024-05-18 14:02:14 +0300

    !

--- src/Insertion.java
+++ src/Insertion.java
@@ -1,2 +1,40 @@
-package PACKAGE_NAME;public class Insertion {
+public class Insertion {
+
+    public static void sort(Comparable[] a) {
+        int n = a.length;
+        for (int i = 1; i < n; i++) {
+            for (int j = i; j > 0 && less(a[j], a[j - 1]); j--) {
+                exch(a, j, j - 1);
+            }
+        }
+    }
+
+    private static boolean less(Comparable v, Comparable w) {
+        return v.compareTo(w) < 0;
+    }
+
+    private static void exch(Comparable[] a, int i, int j) {
+        Comparable temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
+
+    public static void main(String[] args) {
+        Integer[] a = {6, 3, 8, 0, 4, 9};
+
+        System.out.println("Before sorting:");
+        printArray(a);
+
+        sort(a);
+
+        System.out.println("\nAfter sorting:");
+        printArray(a);
+    }
+
+    private static void printArray(Comparable[] a) {
+        for (Comparable item : a) {
+            System.out.print(item + " ");
+        }
+        System.out.println();
+    }
 }
